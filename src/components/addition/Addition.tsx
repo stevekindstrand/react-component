f461c07119732b2d30e485fe745b081143d8c4fc
stevekindstrand <stevekindstrand@gmail.com>
2022-03-14 08:08:17 +0100

    initial commit

---
+++ src/components/addition/Addition.tsx
@@ -0,0 +1,13 @@
+import './Addition.css'
+
+export function Addition() {
+    return (
+        <div>
+            <input></input>
+            <span> + </span>
+            <input></input>
+            <button>addera</button>
+            <p>= </p> 
+        </div>
+    );
+}
